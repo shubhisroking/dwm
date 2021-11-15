811a9b5c2c31b0244a3bd08b21b3d29428be5b3b
BrunoCooper17 <BrunoCooper17@outlook.com>
2021-11-15 14:04:53 -0600

    MoveStack patch
    This plugin allows you to move clients around in the stack and swap them
    with the master. It emulates the behavior off mod+shift+j and mod+shift+k
    in Xmonad. movestack(+1) will swap the client with the current focus with
    the next client. movestack(-1) will swap the client with the current focus
    with the previous client.

--- config.def.h
+++ config.def.h
@@ -69,6 +69,7 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *flamcmd[] = { "flameshot", "gui", NULL };
 #include <X11/XF86keysym.h>
+#include "movestack.c"
 #define TERMINAL "st"
 
 static const Key keys[] = {
@@ -82,6 +83,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
