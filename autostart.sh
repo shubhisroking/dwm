5a3216365883c5b312af1e4a242001a8c40a7859
Shubh <shubhisroking@proton.me>
2023-09-05 13:28:46 +0530

    add autostart.sh
    Signed-off-by: Shubh <shubhisroking@proton.me>

---
+++ autostart.sh
@@ -0,0 +1,9 @@
+xrdb -merge ~/.Xresources &
+picom &
+redshift -P -O 5000 & 
+dunst &
+feh --bg-scale ~/.walls/10-12.jpg &
+exec xautolock -time 10 -locker slock &
+exec clipmenud &
+slstatus &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
