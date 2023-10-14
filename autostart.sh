2fcfdb6802a2adebf77fdfa0196b32a1a0b21ae8
Shubh <shubhisroking@proton.me>
2023-10-14 16:07:33 +0530

    use nitrogen instead of feh
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@ xrdb -merge ~/.Xresources &
 picom &
 redshift -P -O 5000 & 
 dunst &
-feh --bg-scale ~/.walls/10-12.jpg &
+nitrogen --restore &
 exec xautolock -time 10 -locker slock &
 greenclip daemon &
 slstatus &
