90fc267ed1ec3fa0f62b11287b6a7f2923c07097
Shubh <shubhisroking@proton.me>
2023-10-15 17:27:22 +0530

    add some comments and modify autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,21 @@
+#!/bin/sh
+
+# Merge X resources
 xrdb -merge ~/.Xresources &
+
+# Start the compositor, adjust screen color temperature, and enable screen locking
 picom &
-redshift -P -O 5000 & 
+redshift -P -O 5000 &
+xautolock -time 10 -locker slock &
+
+# Start the notification daemon, clipboard manager, status bar, and set wallpaper
 dunst &
-nitrogen --restore &
-exec xautolock -time 10 -locker slock &
 greenclip daemon &
 slstatus &
+nitrogen --restore &
+
+# Start the Polkit-GNOME authentication agent
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Set the keyboard repeat rate
 xset r rate 300 50
